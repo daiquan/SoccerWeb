4e51c7d8f128442de5c1f47e81e0a97b5b878c75
zhanggeng218@gmail.com <zg19870218>
2016-01-01 18:06:08 -0600

    Added Player, Team and League models.

---
+++ SoccerWeb/SoccerWeb/SoccerWeb/Models/League.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SoccerWeb.Models
+{
+    public class League
+    {
+        public int ID { get; set; }
+        public string LeagueName { get; set; }
+    }
+}
---
+++ SoccerWeb/SoccerWeb/SoccerWeb/Models/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SoccerWeb.Models
+{
+    public class Player
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Gender { get; set; }
+        public int TeamID { get; set; }
+    }
+}
