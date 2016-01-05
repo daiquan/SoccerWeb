cba80b32b8a184d871380afec78aa9177657ced3
zhanggeng218@gmail.com <zg19870218>
2016-01-05 14:39:21 -0600

    Created TeamService interface for dependecy injection.

---
+++ SoccerWeb/SoccerWeb/SoccerWeb/ModelServices/TeamService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using SoccerWeb.DataAccessLayer;
+using SoccerWeb.Models;
+using System.Web.Mvc;
+
+namespace SoccerWeb.ModelServices
+{
+    public interface ITeamService
+    {
+        ActionResult Index();
+        ActionResult Details(int? id);
+        ActionResult Create();
+        ActionResult Create(Team team);
+        ActionResult Edit(int? id);
+        ActionResult Edit(Team team);
+        ActionResult Delete(int? id);
+        ActionResult DeleteConfirmed(int id);
+        ActionResult Dispose(bool disposing);
+    }
+
+    //public class TeamService : ITeamService
+    //{
+    //}
+}
